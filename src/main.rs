a2affc02258bc5337962fdf612476f32935ae4e9
DeCaelo <ludovic.cleuet@gmail.com>
2023-12-17 16:13:51 +0100

    add socketioxide

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use axum::routing::get;
+use socketioxide::{extract::SocketRef, SocketIo};
 use tracing::info;
 use tracing_subscriber::FmtSubscriber;
+
+async fn on_connect(socket: SocketRef) {
+    info!("socket connected: {}", socket.id);
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing::subscriber::set_global_default(FmtSubscriber::default())?;
 
-    let app = axum::Router::new().route("/", get(|| async { "Hello, World" }));
+    let (layer, io) = SocketIo::new_layer();
+
+    io.ns("/", on_connect);
+
+    let app = axum::Router::new()
+        .route("/", get(|| async { "Hello, World" }))
+        .layer(layer);
 
     info!("Starting server");
 
