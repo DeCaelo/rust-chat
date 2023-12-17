6400abe9a21b1c386e7989b58d90be3b0308fda5
DeCaelo <ludovic.cleuet@gmail.com>
2023-12-17 17:17:15 +0100

    MessageStore & RoomStore

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+mod state;
+
 use axum::routing::get;
 use socketioxide::{
-    extract::{Data, SocketRef},
+    extract::{Data, SocketRef, State},
     SocketIo,
 };
 use tower::ServiceBuilder;
@@ -15,44 +17,61 @@ struct MessageIn {
 }
 
 #[derive(serde::Serialize)]
-struct MessageOut {
-    text: String,
-    user: String,
-    date: chrono::DateTime<chrono::Utc>,
+struct Messages {
+    messages: Vec<state::Message>,
 }
 
 async fn on_connect(socket: SocketRef) {
     info!("socket connected: {}", socket.id);
 
-    socket.on("join", |socket: SocketRef, Data::<String>(room)| {
-        info!("Received join: {:?}", room);
-        let _ = socket.leave_all();
-        let _ = socket.join(room);
-    });
+    socket.on(
+        "join",
+        |socket: SocketRef, Data::<String>(room), store: State<state::MessageStore>| async move {
+            info!("Received join: {:?}", room);
+            let _ = socket.leave_all();
+            let _ = socket.join(room.clone());
+            let messages = store.get(&room).await;
+            let _ = socket.emit("messages", Messages { messages });
+        },
+    );
 
-    socket.on("message", |socket: SocketRef, Data::<MessageIn>(data)| {
-        info!("Received message: {:?}", data);
+    socket.on(
+        "message",
+        |socket: SocketRef, Data::<MessageIn>(data), store: State<state::MessageStore>| async move {
+            info!("Received message: {:?}", data);
 
-        let response = MessageOut {
-            text: data.text,
-            user: format!("anon-{}", socket.id),
-            date: chrono::Utc::now(),
-        };
+            let response = state::Message {
+                text: data.text,
+                user: format!("anon-{}", socket.id),
+                date: chrono::Utc::now(),
+            };
 
-        let _ = socket.within(data.room).emit("message", response);
-    })
+            store.insert(&data.room, response.clone()).await;
+
+            let _ = socket.within(data.room).emit("message", response);
+        },
+    )
+}
+
+async fn handler(axum::extract::State(io): axum::extract::State<SocketIo>) {
+    info!("handler called");
+    let _ = io.emit("hello", "world");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing::subscriber::set_global_default(FmtSubscriber::default())?;
 
-    let (layer, io) = SocketIo::new_layer();
+    let messages = state::MessageStore::default();
+
+    let (layer, io) = SocketIo::builder().with_state(messages).build_layer();
 
     io.ns("/", on_connect);
 
     let app = axum::Router::new()
-        .route("/", get(|| async { "Hello, World" }))
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/hello", get(handler))
+        .with_state(io)
         .layer(
             ServiceBuilder::new()
                 .layer(CorsLayer::permissive())
